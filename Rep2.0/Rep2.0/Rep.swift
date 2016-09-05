ffa87a70f0624939b62cd5bb6fbf32c6afb2b50d
Justin Carver <15Jcarver202@gmail.com>
2016-09-05 13:39:59 -0600

    Added all needed files for model and views

---
+++ Rep2.0/Rep2.0/Rep.swift
@@ -0,0 +1,9 @@
+//
+//  Rep.swift
+//  Rep2.0
+//
+//  Created by Justin Carver on 9/5/16.
+//  Copyright © 2016 Justin Carver. All rights reserved.
+//
+
+import Foundation
---
+++ Rep2.0/Rep2.0/RepController.swift
@@ -0,0 +1,9 @@
+//
+//  RepController.swift
+//  Rep2.0
+//
+//  Created by Justin Carver on 9/5/16.
+//  Copyright © 2016 Justin Carver. All rights reserved.
+//
+
+import Foundation
