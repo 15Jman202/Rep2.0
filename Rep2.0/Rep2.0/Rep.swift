f6669de77b78c480174a6a702cae4c68df24fe72
Justin Carver <15Jcarver202@gmail.com>
2016-09-05 14:31:46 -0600

    Made the States appear on initial View and setup segue to transfer to detail view

--- Rep2.0/Rep2.0/Rep.swift
+++ Rep2.0/Rep2.0/Rep.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+class Representative {
+    
+    private let kResultsDictionary = "results"
+    private let kName = "name"
+    private let kState = "state"
+    private let kParty = "party"
+    private let kDistrict = "district"
+    
+    let name: String
+    let state: String
+    let party: String
+    let district: String
+    
+    init?(dictionary: [String: AnyObject]) {
+        guard let name = dictionary[kName] as? String,
+            state = dictionary[kState] as? String,
+            party = dictionary[kParty] as? String,
+            district = dictionary[kDistrict] as? String
+            
+            else { return nil }
+        
+        self.name = name
+        self.district = district
+        self.party = party
+        self.state = state
+    }
+}
--- Rep2.0/Rep2.0/RepController.swift
+++ Rep2.0/Rep2.0/RepController.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+class RepresentativeController {
+    
+    static let baseURL = NSURL(string: "http://whoismyrepresentative.com/getall_reps_bystate.php")
+    
+    static func getReps(state: String, completion: (reps: [Representative]?) -> Void) {
+        
+        guard let url = baseURL else { completion(reps: []); return }
+        
+        let urlParameters = ["state" : "\(state)", "output" : "json"]
+        
+        NetworkController.performRequestForURL(url, httpMethod: .Get, urlParameters: urlParameters, body: nil) { (data, error) in
+            if error != nil {
+                print(error)
+                completion(reps: [])
+                return
+            }
+            
+            guard let data = data, JSONDictionary = (try? NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)) as? [[String: AnyObject]] else { completion(reps: []); return }
+            
+            let reps = JSONDictionary.flatMap({ Representative(dictionary: $0) })
+            
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                completion(reps: reps)
+            })
+        }
+    }
+}
